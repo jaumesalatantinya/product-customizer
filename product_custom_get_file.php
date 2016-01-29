d2ab2ab4c72490f9bfa59578839b7af46dfcfc52
Jaume Sala Tantinya <jaumesalatantinya@gmail.com>
2016-01-29 12:09:11 +0100

    Limit the pdf number pages

--- product_custom_get_file.php
+++ product_custom_get_file.php
@@ -4,10 +4,12 @@
     try {  
         $client = new Pdfcrowd('dani3rs', '6cb8fce0fa072b77c4655a4da2631cb2');
         $apiRequests = new ApiRequests();
+        $numPages = count($apiRequests->getViews($_GET['IDcus']));
         $pageHeight = $apiRequests->getCustomization($_GET['IDcus'])[0]['Height'] * 72 / 96; //pixels to points conversion
         $pdf = $client->setPageWidth(450);
         $pdf = $client->setPageHeight($pageHeight);
         $pdf = $client->setPageMargins(0, 0, 0, 0);
+        $pdf = $client->setMaxPages($numPages);
         $pdf = $client->convertURI('http://www.sellosyrotulos.com/webmaster/product_custom_render_custom.php?IDcus='.$_GET['IDcus'].'&type='.$_GET['type']);
 
         header('Content-Type: application/pdf');
