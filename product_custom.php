20919465ebdc0637dc0c1a93f9c118af39382417
Jaume Sala Tantinya <jaumesalatantinya@gmail.com>
2016-02-10 18:39:46 +0100

    Add front and webmaster logic

--- product_custom.php
+++ product_custom.php
@@ -1,16 +1,32 @@
 <?php
-    require_once('Connections/bd_start.php');
-    $colname_Recordset2 = "-1";
-    if (isset($_GET['IDpro']) && $_GET['IDpro']!='') {
-        $colname_Recordset2 = $_GET['IDpro'];
+    //require_once('Connections/bd_start.php');
+    require_once('../Connections/bd_SELLOS.php');
+    require_once('product-customizer/api/api-requests.php');
+    $apiRequests = new ApiRequests();
+
+    $error = '';
+    $idCustom = 0;
+    $idPro = $_GET['IDpro'];
+    $env = $_GET['env'];
+    $productName = $apiRequests->getProduct($idPro)[0]['Producto_esp'];
+    if (!isset($_GET['env']) || $_GET['env']=='' || !isset($_GET['IDpro']) || $_GET['IDpro']=='' || is_null($productName)) {
+        $error = 'Error Env o IDpro incorrectos';
     }
-    $query_Recordset2 = sprintf("SELECT * FROM bd_productos WHERE IDpro=%s", GetSQLValueString($colname_Recordset2, "int"));
-    $Recordset2 = mysql_query($query_Recordset2, $bd_SELLOS) or die(mysql_error());
-    $row_Recordset2 = mysql_fetch_assoc($Recordset2);
-    $totalRows_Recordset2 = mysql_num_rows($Recordset2);
-    if ($totalRows_Recordset2<1){
-        echo "<script language='Javascript'>location.href='list_product.php';</script>";
-        exit;
+    else {
+        if ($env == 'webmaster') {
+            if (isset($_GET['IDcus']) && $_GET['IDcus']!=''){
+                $idCustom = $_GET['IDcus'];
+            }
+            else {
+                $idCustom = $apiRequests->getTemplateId($idPro)[0]['IDcus'];
+            }
+        }
+        if ($env == 'front') {
+            $idCustom = $apiRequests->getCustomUserId($idPro)[0]['ID_cus'];
+            if (is_null($idCustom)) {
+                $idCustom = $apiRequests->putCustom($idPro);
+            }
+        }
     }
 ?>
 
@@ -23,7 +39,7 @@
     <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css" rel="stylesheet" type="text/css" />
     <link href="product-customizer/vendor/colorpicker/jquery.colorpicker.css" rel="stylesheet" type="text/css" />
     <link href="product-customizer/styles.css" rel="stylesheet" type="text/css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js" type="text/javascript"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js"></script>
     <script src="product-customizer/vendor/colorpicker/jquery.colorpicker.js"></script>
@@ -34,29 +50,17 @@
     <script type="text/javascript"> 
         $(document).ready(function(){
             $.ajaxSetup({ cache: false });
-            var idCustom;
-            var idProd;
-            <?php 
-            if (isset($_GET['IDcus'])){ echo 'idCustom = ' . $_GET['IDcus'] . ';'; }
-            if (isset($_GET['IDpro'])){ echo 'idProd = ' . $_GET['IDpro'] . ';';   }
-            ?>
+            var idCustom = <?=$idCustom?>;
+            var error = '<?=$error?>';
+
             var productCustomizer = new ProductCustomizer();
-            if (idCustom) {
+            if (error != '' || idCustom == 0) {
+                productCustomizer.showMsg('Error', error);
+            }
+            else {
                 productCustomizer.idCustom = idCustom;
                 productCustomizer.init();
             }
-            else if (idProd) {
-                $.ajax('product-customizer/api/api.php?request=get-custom-template-id&IDpro='+idProd)
-                .done(function(custom){
-                    if (custom){
-                        productCustomizer.idCustom = custom[0].IDcus;
-                        productCustomizer.init();
-                    }
-                    else { productCustomizer.showMsg('Error', 'Loading template customization data'); }
-                })
-                .fail(function(){ productCustomizer.showMsg('Error', 'API No template customization to load'); });
-            }
-            else { productCustomizer.showMsg('Error', 'No idCustom to load'); }
         });
     </script>
 </head>
@@ -68,7 +72,7 @@
             <ul id="nav-main"></ul>
         </div>
         <div class="col-2">
-            <h1><?php echo $row_Recordset2['Producto_esp']?></h1>
+            <h1><?=$productName?></h1>
             <div id="wrapper-aux-menu"><div id="aux-menu"></div></div>
             <div id="toast"></div>
             <div id="wrapper-view"><div id="view"></div></div>
