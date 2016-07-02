fbec1019078f98aa335c528f59447f3be94364c6
Jaume Sala Tantinya <jaumesalatantinya@gmail.com>
2016-07-02 15:05:29 +0200

    Mini refactor

--- product-customizer/api/api.php
+++ product-customizer/api/api.php
@@ -36,140 +36,112 @@ class Api {
         switch ($this->request) {
             case 'get-custom-template-id':
                 $this->response = $this->apiRequests->getTemplateId($this->idPro);
-                $this->returnJSONResponse();
+                break;
+            case 'get-custom-user-id':
+                $this->response = $this->apiRequests->getCustomUserId($this->idPro, $this->idProvar, $this->$idCart, $this->$idClient);
                 break;
             case 'get-custom':
                 $this->response = $this->apiRequests->getCustomization($this->idCus, $this->isAdmin);
-                $this->returnJSONResponse();
                 break;
             case 'get-views':
                 $this->response = $this->apiRequests->getViews($this->idCus);
-                $this->returnJSONResponse();
                 break;
             case 'get-view':
                 $this->response = $this->apiRequests->getView($this->idVie);
-                $this->returnJSONResponse();
                 break;
             case 'get-custom-elements':
                 $this->response = $this->apiRequests->getCustomElements($this->idVie);
-                $this->returnJSONResponse();
                 break;
             case 'get-custom-element':
                 $this->response = $this->apiRequests->getCustomElement($this->idCusele);
-                $this->returnJSONResponse();
                 break;
             case 'get-fonts':
                 $this->response = $this->apiRequests->getFonts();
-                $this->returnJSONResponse();
                 break;
             case 'get-svgs':
                 $this->response = $this->apiRequests->getSvgs();
-                $this->returnJSONResponse();
                 break;
             case 'get-product':
                 $this->response = $this->apiRequests->getProduct($this->idPro);
-                $this->returnJSONResponse();
                 break;
             case 'get-product-id':
                 $this->response = $this->apiRequests->getProductId($this->idCus);
-                $this->returnJSONResponse();
                 break;
             case 'get-colors':
                 $this->response = $this->apiRequests->getColors($this->idProtyp);
-                $this->returnJSONResponse();
                 break;
             case 'get-color':
                 $this->response = $this->apiRequests->getColor($this->idProcol);
-                $this->returnJSONResponse();
-                break;
-            case 'get-custom-user-id':
-                $this->response = $this->apiRequests->getCustomUserId($this->idPro, $this->$idCart);
-                $this->returnJSONResponse();
                 break;
 
 
             case 'put-custom':
-                $this->response = $this->apiRequests->putCustom($this->idPro, $this->idCart, $this->idProvar, $this->idClient);
-                $this->returnJSONResponse();
+                $this->response = $this->apiRequests->putCustom($this->idPro, $this->idProvar, $this->idCart, $this->idClient);
                 break;
             case 'put-view':
                 $this->response = $this->apiRequests->putView($this->idCus);
-                $this->returnJSONResponse();
                 break;
             case 'put-img-to-view':
                 $this->response = $this->apiRequests->putImgToView($this->idVie, $this->file);
-                $this->returnJSONResponse();
                 break;
             case 'put-area':
                 $this->response = $this->apiRequests->putArea($this->idVie);
-                $this->returnJSONResponse();
                 break;
             case 'put-text':
                 $this->response = $this->apiRequests->putText($this->idVie, $this->zindex);
-                $this->returnJSONResponse();
                 break;
             case 'put-img':
                 $this->response = $this->apiRequests->putImg($this->idVie, $this->file, $this->zindex);
-                $this->returnJSONResponse();
                 break;
             case 'put-svg':
                 $this->response = $this->apiRequests->putSvg($this->idVie, $this->idSvg, $this->zindex);
-                $this->returnJSONResponse();
                 break;
 
 
 
             case 'update-custom-element-pos-size':
                 $this->response = $this->apiRequests->updateCustomElementPosSize($this->idCusele, $this->data);
-                $this->returnJSONResponse();
                 break;
             case 'update-custom-element-zindex':
                 $this->response = $this->apiRequests->updateCustomElementZindex($this->idCusele, $this->zindex);
-                $this->returnJSONResponse();
                 break;
             case 'update-area':
                 $this->response = $this->apiRequests->updateArea($this->idCusele, $this->data);
-                $this->returnJSONResponse();
                 break;
             case 'update-text':
                 $this->response = $this->apiRequests->updateText($this->idCusele, $this->data);
-                $this->returnJSONResponse();
                 break; 
             case 'update-text-attr':
                 $this->response = $this->apiRequests->updateTextAttr($this->idCusele, $this->data);
-                $this->returnJSONResponse();
                 break;
             case 'update-color':
                 $this->response = $this->apiRequests->updateColor($this->idCus, $this->idProcol);
-                $this->returnJSONResponse();
                 break;
             case 'update-height':
                 $this->response = $this->apiRequests->updateHeight($this->idCus, $this->data);
-                $this->returnJSONResponse();
                 break;
             case 'update-is-modified-from-template':
                 $this->response = $this->apiRequests->updateIsModifiedFromTemplate($this->idCus);
-                $this->returnJSONResponse();
                 break;
                           
 
 
             case 'del-custom':
                 $this->response = $this->apiRequests->delCustom($this->idCus);
-                $this->returnJSONResponse();
                 break;
             case 'del-view':
                 $this->response = $this->apiRequests->delView($this->idVie, true);
-                $this->returnJSONResponse();
                 break;
             case 'del-custom-element':
                 $this->response = $this->apiRequests->delCustomElement($this->idCusele);
-                $this->returnJSONResponse();
                 break;
+
+
 
             default:
                 echo 'API PRODUCT CUSTOMIZER';
         }
+        $this->returnJSONResponse();
     }
 
 
